52f4336b363d68ade4ae1f5e848023405dce1680
Daniel Resino <thedan296@gmail.com>
2024-12-27 21:09:41 +0100

    start.html and script.js changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,86 @@
 /* Script JS Api Use */
 
+// First Api Search
+
+let apiKey = "yVhAb1oXDUGASbyAMclGTte4wQosdlTc";
+
+const apiRequest = async() => {
+    const api = await fetch("https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=" + apiKey + "&keyword=NFL");
+    const data = await api.json();
+    return data;
+}
+
+let eventtable = document.getElementById("eventtable");
+const arrayEvent = [];
+
+const loadApiFootball = async() => {
+    const football = await apiRequest();
+    console.log(football);
+    
+    let arrayEvents = football._embedded.events;
+    arrayEvents.forEach(event => {
+        let newarticle = document.createElement("article");
+        newarticle.classList.add("p-6")
+        newarticle.classList.add("rounded-sm")
+        newarticle.classList.add("bg-blue-300")
+        newarticle.classList.add("w-56")
+        newarticle.classList.add("flex");
+        newarticle.classList.add("flex-col")
+        let newimg = document.createElement("img");
+        newimg.src = event.images[1].url;
+        newimg.classList.add("object-cover")
+        newimg.classList.add("rounded-md")
+        let newtitle = document.createElement("h2");
+        newtitle.textContent = event.name;
+        let newp = document.createElement("p");
+        newarticle.appendChild(newimg);
+        newarticle.appendChild(newtitle);
+        newarticle.appendChild(newp);
+        eventtable.appendChild(newarticle);
+
+        arrayEvent.push(newarticle);
+
+    });
+    
+}
+
+// Second Api Search
+
+const apiRequest2 = async() => {
+    const api = await fetch("https://countriesnow.space/api/v0.1/countries/states");
+    const data = await api.json();
+    return data;
+}
+
+let locations = document.getElementById("locations");
+
+const loadApiStates = async() => {
+    const data = await apiRequest2();
+    const arrayCountries = data.data;
+    const states = arrayCountries.filter(element => element.name === "United States").map(state => {return state.states});
+    states[0].forEach(state => {
+        let newOption = document.createElement("OPTION");
+        newOption.value = state.name;
+        newOption.textContent = state.name;
+        locations.append(newOption);
+    })
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadApiFootball();
+    loadApiStates();
+})
+
 // Variables
 
 let navbutton = document.getElementById("navbutton");
 let smallmenu = document.getElementById("smallmenu");
 
 let open = false;
+
+// Open Nav Event
 
 navbutton.addEventListener("click", () => {
     if(open){
@@ -19,43 +94,17 @@ navbutton.addEventListener("click", () => {
     }
 })
 
-// Api Search
+// Filter Events By Team Event
 
-let apiKey = "yVhAb1oXDUGASbyAMclGTte4wQosdlTc";
+const filterEventTeam = () => {
 
-const apiRequest = async() => {
-    const api = await fetch("https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=" + apiKey + "&keyword=NFL");
-    const data = await api.json();
-    return data;
 }
 
-let eventtable = document.getElementById("eventtable");
+// Filter Events By Location Event
 
-const loadApiFootball = async() => {
-    const football = await apiRequest();
-    console.log(football);
-    
-    let arrayEvents = football._embedded.events;
-    arrayEvents.forEach(event => {
-        let newarticle = document.createElement("article");
-        newarticle.classList.add("w-36")
-        let newimg = document.createElement("img");
-        newimg.src = event.images[1].url;
-        newimg.classList.add("object-cover")
-        let newtitle = document.createElement("h2");
-        let newp = document.createElement("p");
-        
-
-        newarticle.appendChild(newimg);
-        newarticle.appendChild(newtitle);
-        newarticle.appendChild(newp);
-        eventtable.appendChild(newarticle);
-    });
+const filterEventLocation = () => {
     
 }
-
-loadApiFootball();
-
 
 
 
